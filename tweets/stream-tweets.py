70e211c764a83184ea631f23061451e3adde2a6f
Casey <casey.key@oracle.com>
2019-06-26 11:51:02 -0400

    Need to fetch historical tweets

--- tweets/stream-tweets.py
+++ tweets/stream-tweets.py
@@ -1,17 +1,25 @@
-import csv
-import json
-from twython import TwythonStreamer
+import csv # Exporting tweets
+import datetime # Calculate rate of tweets
+import json # Loading twitter credentials
+import sys # For keyword 'track' arguments
+from twython import TwythonStreamer # Gateway to Twitter
+
+# Track filters can be passed as arguments
+argv = sys.argv
+
 
 # Load Twitter API credentials
 with open("twitter-creds.json", "r") as f:
     creds = json.load(f)
 
+# Used for sanitizing input for ADW
 def deEmojify(inputString):
     return inputString.encode('ascii', 'ignore').decode('ascii')
 
 # Filter out unwanted data
 def process_tweet(tweet):
     d = {}
+    d['date'] = tweet['created_at']
     d['hashtags'] = [hashtag['text'] for hashtag in tweet['entities']['hashtags']]
     d['text'] = deEmojify(tweet['text']).replace("\n", " ")
     d['user'] = tweet['user']['screen_name']
@@ -20,14 +28,39 @@ def process_tweet(tweet):
 
 # Create a class that inherits TwythonStreamer
 class MyStreamer(TwythonStreamer):
+    # start_time = None
+    # last_tweet_time = None
+    # total_tweets = None
+    # total_difference = None
+    
+
+    def __init__(self, *creds):
+        self.start_time = datetime.datetime.now()
+        self.last_tweet_time =  self.start_time
+        self.total_tweets = 0
+        self.total_difference = 0
+        super().__init__(*creds)  
 
     # Received data
     def on_success(self, data):
-
+            
+        
         # Only collect tweets in English
         if data['lang'] == 'en':
+            self.total_tweets += 1
+            # Calculate average time per tweet
+            tweet_time = datetime.datetime.now()
+            tweet_time_difference = tweet_time - self.last_tweet_time
+            self.total_difference += tweet_time_difference.total_seconds()
+            avg_time_per_tweet = self.total_difference / self.total_tweets
+            self.last_tweet_time = tweet_time
+            
+            # Extract tweet and append to file
             tweet_data = process_tweet(data)
             self.save_to_csv(tweet_data)
+            
+            # Update stream status to console
+            print(avg_time_per_tweet, "t/s;", tweet_data['text'])
 
     # Problem with the API
     def on_error(self, status_code, data):
@@ -36,7 +69,6 @@ class MyStreamer(TwythonStreamer):
 
     # Save each tweet to csv file
     def save_to_csv(self, tweet):
-        print(tweet.keys())
         with open(r'saved_tweets.csv', 'a') as f:
             if f.tell() == 0:
                 header = list(tweet.keys())
@@ -51,5 +83,19 @@ class MyStreamer(TwythonStreamer):
 stream = MyStreamer(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'],
                     creds['ACCESS_KEY'], creds['ACCESS_SECRET'])
 
-# Start the stream
-stream.statuses.filter(track='Oracle')
+# Determine and print filters
+tracks = 'oracle' # Track filters are not case-sensitive 
+print("Streaming tweets about:", tracks)
+if len(argv) != 1:
+    seperator = ', '
+    tracks = seperator.join(argv[1:])
+    for track in argv[1:]:
+        print(">", track)
+else:
+    print(">", tracks)
+
+try:
+    # Start the stream
+    stream.statuses.filter(track=tracks)
+except (KeyboardInterrupt, SystemExit):
+    print("Saved", stream.total_tweets, "tweets in", stream.start_time - datetime.datetime.now())
